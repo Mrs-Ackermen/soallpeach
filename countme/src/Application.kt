9da0e53d7c93a5c028ecc2a44272dd3dffc5fdf6
Mrs-Ackermen <eshq_2@yahoo.com>
2020-04-28 23:50:56 +0430

    add coroutines

--- countme/src/Application.kt
+++ countme/src/Application.kt
@@ -9,24 +9,42 @@ import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import kotlinx.coroutines.*
+// import kotlinx.html.*
+// import io.ktor.features.*
+// import io.ktor.html.*
+// import java.util.*
+// import kotlin.system.*
 
-
+val compute = newSingleThreadContext("Single thread")
+var jobs : ArrayList<Deferred<Unit>> = arrayListOf()
+var sumOfRequests = 0
 fun main(args: Array<String>): Unit {
     val port = System.getenv("PORT")?.toInt() ?: 80
     embeddedServer(Netty, port = port, module = Application::mainModule).start(wait = true)
 }
 
-var sumOfRequests = 0
-
 fun Application.mainModule() {
     routing {
         post("/") {
             val receiveNum = call.receive<String>()
-            sumOfRequests += receiveNum.toInt()
+            call.respondHandlingLongCalculation(receiveNum)
         }
         get("/count") {
-            call.respondText("$sumOfRequests", ContentType.Text.Plain)
+            call.respondLatestNumber()
         }
     }
 }
 
+private suspend fun ApplicationCall.respondHandlingLongCalculation(receiveNum : String) {
+    jobs.add(GlobalScope.async(compute) {
+        sumOfRequests += receiveNum.toInt()
+    })
+}
+
+private suspend fun ApplicationCall.respondLatestNumber(){
+    
+    jobs.awaitAll()
+    respondText("$sumOfRequests", ContentType.Text.Plain)   
+    
+}
