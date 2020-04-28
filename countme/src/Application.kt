57c8f2258302108dddcc85ae8f1e69f1c34ae557
Mrs-Ackermen <a.samawi@roundtableapps.com>
2020-04-28 11:23:37 +0430

    change application's server

--- countme/src/Application.kt
+++ countme/src/Application.kt
@@ -7,24 +7,29 @@ import io.ktor.request.*
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit {
+    val server = embeddedServer(Netty, port = 80) {
+        routing {
+            post("/") {
+                val receiveNum = call.receive<String>()
+                sumOfRequests += receiveNum.toInt()
+                call.respondText("$sumOfRequests")
+            }
+            get("/count") {
+                call.respondText("$sumOfRequests", ContentType.Text.Plain)
+            }
+        }
+    }
+    server.start(wait = true)
+}
 
 var sumOfRequests = 0
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
-    routing {
-        post("/") {
-            val receiveNum = call.receive<String>()
-            sumOfRequests += receiveNum.toInt()
-            call.respondText("$sumOfRequests")
-        }
-        get("/count") {
-            call.respondText("$sumOfRequests", ContentType.Text.Plain)
-        }
-    }
-}
+fun Application.module(testing: Boolean = false) {}
 
