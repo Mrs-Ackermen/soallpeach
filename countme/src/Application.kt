9f2aba591ba4dac659e08ea7db20703f639f5369
Mrs-Ackermen <a.samawi@roundtableapps.com>
2020-04-28 15:19:33 +0430

    fix some issues

--- countme/src/Application.kt
+++ countme/src/Application.kt
@@ -12,23 +12,21 @@ import io.ktor.server.netty.Netty
 
 
 fun main(args: Array<String>): Unit {
-    val server = embeddedServer(Netty, port = 80) {
-        routing {
-            post("/") {
-                val receiveNum = call.receive<String>()
-                sumOfRequests += receiveNum.toInt()
-            }
-            get("/count") {
-                call.respondText("$sumOfRequests", ContentType.Text.Plain)
-            }
-        }
-    }
-    server.start(wait = true)
+    val port = System.getenv("PORT")?.toInt() ?: 80
+    embeddedServer(Netty, port = port, module = Application::mainModule).start(wait = true)
 }
 
 var sumOfRequests = 0
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {}
+fun Application.mainModule() {
+    routing {
+        post("/") {
+            val receiveNum = call.receive<String>()
+            sumOfRequests += receiveNum.toInt()
+        }
+        get("/count") {
+            call.respondText("$sumOfRequests", ContentType.Text.Plain)
+        }
+    }
+}
 
